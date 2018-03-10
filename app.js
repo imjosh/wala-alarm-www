b0e9b2bba05b85623b1daaa39efff259ca979238
Joshua Newell <joshuanewell@gmail.com>
2018-03-10 15:43:23 -0500

    log listening port

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //local test server
 var express = require('express');
 var app = express();
+
+const PORT = 8000;
 app.use(express.static(__dirname + '/dist'));
-app.listen(8000);
+app.listen(PORT);
+console.log('Listening on http://127.0.0.1:' + PORT);
 
